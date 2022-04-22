288d3b7f91d9e34f527d9827b2ef156909cae33f
abhikeshri10 <60318491+abhikeshri10@users.noreply.github.com>
2022-04-23 02:09:31 +0530

    Admin changes added

--- frontend/src/Components/ADMIN/AdminRoute.js
+++ frontend/src/Components/ADMIN/AdminRoute.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './Login';
-
+import AddTrain from './AddTrain';
+import AddStation from './AddStation';
+import EditSchedule from './EditSchedule';
 function AdminRoute() {
-    return (
-        <div>
-			<Routes>
-				<Route path='login' element={<Login/>} />
-			</Routes>
-		</div>
-    )
+  return (
+    <div>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='addtrain' element={<AddTrain />} />
+        <Route path='addStation' element={<AddStation />} />
+        <Route path='editSchedule' element={<EditSchedule />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default AdminRoute
+export default AdminRoute;
