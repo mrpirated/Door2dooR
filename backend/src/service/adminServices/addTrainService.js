8821123af409438a3225be81daa7f710c8617853
Deepesh Rathi <deepeshrathi5050@gmail.com>
2022-04-23 00:12:20 +0530

    add train function created

---
+++ backend/src/service/adminServices/addTrainService.js
@@ -0,0 +1,26 @@
+import dbg from "debug";
+const debug = dbg("service:admin/addStation");
+import checkTrainExists from "../../data/checkTrainExists";
+import checkToken from "../../controllers/checkToken";
+import addTrain from "../../data/addTrain";
+const addStationService = async (token, { num, name, days, schedule }) => {
+	return await checkToken(token)
+		.then((response) => {
+			if (response.data.decoded.type !== "admin") {
+				return Promise.reject({ success: false, message: "Not Authorized" });
+			}
+			return checkTrainExists(num);
+		})
+		.then((response) => {
+			if (response.success) {
+				return addTrain(num, name, days);
+			} else {
+				return Promise.reject({ success: false, message: response.message });
+			}
+		})
+		.catch((err) => {
+			debug(err);
+			return err;
+		});
+};
+export default addStationService;
