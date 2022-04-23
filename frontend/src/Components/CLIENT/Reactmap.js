15f98d040bfcb4db1bec074a97d7b1c9694f8644
Aditya Darji <darjiaditya2000@gmail.com>
2022-04-23 17:30:50 +0530

    track.js almost done

--- frontend/src/Components/CLIENT/Reactmap.js
+++ frontend/src/Components/CLIENT/Reactmap.js
@@ -7,8 +7,10 @@ function Reactmap(props) {
     anchor={[28.879, 77.6997]}
   */
   return (
-    <Map height={500} defaultCenter={[props.defaultLat, props.deafaultLng]} defaultZoom={11}>
-      {/* <Marker width={30} anchor={[28.879, 77.6997]} /> */}
+    // <Map height={300} defaultCenter={[28.879, 77.6997]} defaultZoom={11}>
+    //   <Marker width={50} anchor={[50.879, 4.6997]} />
+    // </Map>
+    <Map height={500} defaultCenter={[props.defaultLat, props.defaultLng]} defaultZoom={9}>
       {
         props.markers.map((m) => {
           console.log(m);
