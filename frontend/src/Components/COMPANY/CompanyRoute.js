1538204c8ca60625b8e63a66c8213a7ebfc451c4
Aditya Darji <darjiaditya2000@gmail.com>
2022-04-23 22:10:52 +0530

    minor changes in login component and navigation introduced in company

--- frontend/src/Components/COMPANY/CompanyRoute.js
+++ frontend/src/Components/COMPANY/CompanyRoute.js
@@ -4,9 +4,11 @@ import Login from './Login';
 import Signup from './Signup';
 import Order from './Orders' 
 import Track from '../COMMON/Track';
+import Navigation from './Navigation';
 function CompanyRoute() {
     return (
         <div>
+			<Navigation/>
 			<Routes>
 				<Route path='login' element={<Login/>} />
 				<Route path='signup' element={<Signup/>} />
