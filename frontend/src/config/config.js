b3bd881cf4da1009626a2afa76159ffa76c4cd2a
Deepesh Rathi <deepeshrathi5050@gmail.com>
2022-04-24 07:25:40 +0530

    token verification done

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -3,19 +3,21 @@
 
 // console.log(process.env);
 const config = {
-  baseUrl: 'http://localhost:6874/api',
-  position_stack_key: 'b108873b531f412bee73c342a818d23e',
-  token: '/token',
-  company: '/company',
-  client: '/client',
-  admin: '/admin',
-  login: '/login',
-  signup: '/signup',
-  addStation: '/addStation',
-  CLIENT: 'client',
-  COMPANY: 'company',
-  ADMIN: 'admin',
-  pincodeurl: 'http://api.positionstack.com/v1/forward?access_key=b108873b531f412bee73c342a818d23e&query=',
-  findFastestRoute: '/findFastestRoute'
+	baseUrl: "http://localhost:6874/api",
+	position_stack_key: "b108873b531f412bee73c342a818d23e",
+	token: "/token",
+	company: "/company",
+	client: "/client",
+	admin: "/admin",
+	login: "/login",
+	signup: "/signup",
+	addStation: "/addStation",
+	CLIENT: "client",
+	COMPANY: "company",
+	ADMIN: "admin",
+	token: "/token",
+	pincodeurl:
+		"http://api.positionstack.com/v1/forward?access_key=b108873b531f412bee73c342a818d23e&query=",
+	findFastestRoute: "/findFastestRoute",
 };
 export default config;
